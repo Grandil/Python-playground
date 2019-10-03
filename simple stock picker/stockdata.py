76034096ab055590d3c1dd003c43c948c6d6e867
Grandil <a.hoejbjerg.jensen@gmail.com>
2019-10-03 22:05:57 +0200

    Added some variables

--- simple stock picker/stockdata.py
+++ simple stock picker/stockdata.py
@@ -1,16 +1,22 @@
 # Import yfinance
 import yfinance as yf  
  
+interval = "1m"
+priceAtOpenIndex = 0
+priceToCompareIndex = 1
+priceAtSellpointIndex = 15
+ 
 # Get the data for the stock Apple by specifying the stock ticker, start date, and end date
-data = yf.download('AAPL','2019-10-02','2019-10-03', interval="5m")
-print(data.Close[0])
-print(data.Close[1])
-print(data.Close[30])
+data = yf.download('AAPL','2019-09-05','2019-09-06', interval=interval)
+print(data.Close[priceAtOpenIndex])
+print(data.Close[priceToCompareIndex])
+print(data.Close[priceAtSellpointIndex])
  
- 
-if data.Close[1]>data.Close[0] and data.Close[30]>data.Close[1]:
-    profitPerStock = data.Close[30]-data.Close[1]
-    print("Trade was profitable with " + str(profitPerStock) + "profit pr. stock")
+if data.Close[priceToCompareIndex]>data.Close[priceAtOpenIndex]:
+    print("Price had risen at compare point.")
+if data.Close[priceToCompareIndex]>data.Close[priceAtOpenIndex] and data.Close[priceAtSellpointIndex]>data.Close[1]:
+    profitPerStock = data.Close[priceAtSellpointIndex]-data.Close[priceToCompareIndex]
+    print("Trade was profitable with " + str(profitPerStock) + " profit pr. stock")
 else:
     print("Trade was not profitable.")
 # Plot the close prices
